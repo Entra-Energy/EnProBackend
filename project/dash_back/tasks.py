67890ee9a2257cdf41f3e13ffc7a4b55cf5b2bba
Georgi <georgi.manev@entra.energy>
2025-08-27 15:12:03 +0300

    first commit

--- project/dash_back/tasks.py
+++ project/dash_back/tasks.py
@@ -3,7 +3,7 @@
 from celery.utils.log import get_task_logger # type: ignore
 from celery import shared_task #type: ignore
 
-from dash_back.utils import timeSet, manage_comm, resample_range_task
+from dash_back.utils import timeSet, manage_comm, resample_range_task, _normalize_resample_format, cache_version_for_today
     
 from dash_back.models import Post
 import paho.mqtt.publish as publish
